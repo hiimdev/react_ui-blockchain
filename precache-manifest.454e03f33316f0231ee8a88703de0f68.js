dfe3ee5e71d5749edd9e59cd3ea06560ef9e142a
huycan <vohuy21032001@gmail.com>
2022-07-09 18:52:16 +0700

    Updates

---
+++ precache-manifest.454e03f33316f0231ee8a88703de0f68.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1d80525e416231d2a87",
+    "url": "./static/css/main.04355cb2.chunk.css"
+  },
+  {
+    "revision": "c1d80525e416231d2a87",
+    "url": "./static/js/main.c1d80525.chunk.js"
+  },
+  {
+    "revision": "6336b426631f80f6c7a2",
+    "url": "./static/js/1.6336b426.chunk.js"
+  },
+  {
+    "revision": "06b655263c0d64a65fe2",
+    "url": "./static/js/2.06b65526.chunk.js"
+  },
+  {
+    "revision": "264a31501b7fc08a2dde",
+    "url": "./static/js/runtime~main.264a3150.js"
+  },
+  {
+    "revision": "4a0cd5ac85e0a54ec1c49251cd50ca85",
+    "url": "./static/media/GridBase.4a0cd5ac.png"
+  },
+  {
+    "revision": "7208e93d940a312e50ca75ad85256ff2",
+    "url": "./static/media/Path0.7208e93d.png"
+  },
+  {
+    "revision": "e2bb5534ec442bf98f5cd9fbebba77cc",
+    "url": "./static/media/Path1.e2bb5534.png"
+  },
+  {
+    "revision": "35a9a18e2253edab5266883d788f5d6b",
+    "url": "./static/media/Path2.35a9a18e.png"
+  },
+  {
+    "revision": "36ffcd258685a2732b0fbe293663ff58",
+    "url": "./static/media/Path3.36ffcd25.png"
+  },
+  {
+    "revision": "3ee7ef2eba8cd09d86caa95ff485474f",
+    "url": "./index.html"
+  }
+];
